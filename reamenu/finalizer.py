3e056e825bd191f3cfc9270d69f8733d81e772a3
RCJacH <RCJacH@outlook.com>
2021-01-18 21:45:38 +0800

    add finalizer.py

---
+++ reamenu/finalizer.py
@@ -0,0 +1,23 @@
+import os
+import pathlib
+
+from reamenu.file_parser import FileParser
+
+
+class Finalizer(object):
+    def __init__(self, folderpath, lang='en'):
+        self.folder = pathlib.Path(folderpath)
+        self.src_folder = self.folder.parent / 'src'
+        self.release_path = self.folder.parent / 'release' / 'reaper-menu.ini'
+        self.all_files = [x for x in self.get_all_files() if x.lang == lang]
+
+    def __call__(self):
+        with open(self.release_path, 'w', encoding='utf-8') as release_file:
+            all_lines = []
+            for each_file in self.all_files:
+                all_lines += each_file()
+                all_lines += ('\n',)
+            release_file.write('\n'.join(all_lines))
+
+    def get_all_files(self):
+        return (FileParser(f) for f in self.src_folder.glob(r'./**/*.txt'))
